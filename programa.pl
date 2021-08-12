d3ccde4e6fbe4748d1b3cbe4fa3291675ed97b05
Lucas Barrientos <lbarrientos@frba.utn.edu.ar>
2021-08-12 10:24:24 -0300

    puntos de la parte 2

--- programa.pl
+++ programa.pl
@@ -51,5 +51,84 @@ amistoso(Nombre):-
     mago(Nombre,Caracteristicas,_,_),
     member(amistad,Caracteristicas).
     
-    
+% ------------------------------------------------------------- ELIPSIS ------------------------------------------------------------------------------------------------------------------
+% Parte 2
 
+esDe(hermione, gryffindor).
+esDe(ron, gryffindor).
+esDe(harry, gryffindor).
+esDe(draco, slytherin).
+esDe(luna, ravenclaw).
+
+accionRealizada(harry,irFueraDeCama).
+%accionRealizada(hermione,irAlTercerPiso).
+accionRealizada(hermione,irSeccionRestringidaBiblioteca).
+accionRealizada(harry,irAlBosque).
+accionRealizada(harry,irAlTercerPiso).
+accionRealizada(draco,irALasMazmorras).
+accionRealizada(ron,ganarAjedrezMagico).
+accionRealizada(hermione,salvarAmigosDeMuerte).
+accionRealizada(harry,ganarAVoldemort).
+accionRealizada(hermione,respondio(dondeSeEncuentraUnBezoar,20,snape)).
+accionRealizada(hermione,respondio(comoHacerLevitarUnaPluma,25,flitwick)).
+
+puntaje(irFueraDeCama,-50).
+puntaje(irAlTercerPiso,-75).
+puntaje(irAlBosque,-50).
+puntaje(irSeccionRestringidaBiblioteca,-10).
+puntaje(ganarAjedrezMagico,50).
+puntaje(salvarAmigosDeMuerte,50).
+puntaje(ganarAVoldemort,60).
+puntaje(respondio(_,Dificultad,snape),Puntos):-
+    Puntos is Dificultad / 2.
+puntaje(respondio(_,Dificultad,Profesor),Dificultad):-
+    Profesor \= snape.
+
+% Punto 1
+% A)
+buenAlumno(Nombre):-
+    accionRealizada(Nombre,_),
+    forall(accionRealizada(Nombre,Accion),not(malaAccion(Accion))).
+
+malaAccion(Accion):-
+    puntaje(Accion,Puntos),
+    Puntos < 0.
+
+% B)
+recurrente(Accion):-
+    accionRealizada(Mago1,Accion),
+    accionRealizada(Mago2,Accion),
+    Mago1 \= Mago2.
+
+% Punto 2
+
+puntosAcumulados(Casa,PuntajeTotal):-
+    findall(PuntosAlumno,puntosDeMiembro(Casa,PuntosAlumno),Puntos),
+    sum_list(Puntos, PuntajeTotal).
+
+puntosDeMiembro(Casa,PuntosAlumno):-
+    esDe(Nombre,Casa),
+    accionRealizada(Nombre,Accion),
+    puntaje(Accion,PuntosAlumno).
+
+% Punto 3
+
+casaGanadora(Casa):-
+    casa(Casa),
+    forall(casasRivales(Casa,CasaRival),tieneMasPuntos(Casa,CasaRival)).
+
+casasRivales(Casa,CasaRival):-
+    casa(CasaRival),
+    Casa \= CasaRival.
+
+casa(Casa):-
+    casaApropiada(Casa,_).
+
+tieneMasPuntos(Casa,CasaRival):-
+    puntosAcumulados(Casa,PuntosCasa),
+    puntosAcumulados(CasaRival,PuntosCasaRival),
+    PuntosCasa > PuntosCasaRival.
+
+% Punto 4
+
+% agregue las lineas 72,73,82,83,84 y 85
